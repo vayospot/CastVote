ca4ff1be0c0f9815527b9e132d2c65df4925e0c4
Vayo <ayomipo185@gmail.com>
2024-11-05 09:11:00 +0100

    ⬇️ Rollback nativewind to v2.0.11 to fix className rendering bug

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import "../global.css";
 
 SplashScreen.preventAutoHideAsync();
 
